fed5d4148a1e3501b06ae4315f0ace0280eed0f8
Tiago Gomes <tidgomes@gmail.com>
2021-12-29 13:30:59 -0300

    creation of the api routes and addition to the server file

--- server.js
+++ server.js
@@ -12,4 +12,9 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
