1a99de206a9680a2cf58081250051f8031aedde6
Tiago Gomes <tidgomes@gmail.com>
2021-12-29 11:12:45 -0300

    environment setup and simple server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
