708f3a0683957da47c7de490fa435358983223a6
Tiago Gomes <tidgomes@gmail.com>
2022-01-06 14:14:17 -0300

    add a post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => res.send("posts route"));
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+const auth = require("../../middleware/auth");
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Posts can not be empty. Plese write a comment")
+        .not()
+        .isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
