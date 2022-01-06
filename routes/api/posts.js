ec24e6bac47e0bc90d19939964350bd3949f52bc
Tiago Gomes <tidgomes@gmail.com>
2022-01-06 15:48:59 -0300

    delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,6 +8,35 @@ const User = require("../../models/User");
 
 const auth = require("../../middleware/auth");
 
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+router.get("/:post_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.log(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.status(500).send("Server error");
+  }
+});
+
 router.post(
   "/",
   [
@@ -43,5 +72,27 @@ router.post(
     }
   }
 );
+
+router.delete("/:post_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    await post.remove();
+    res.json({ msg: "Post removed" });
+  } catch (error) {
+    console.log(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
