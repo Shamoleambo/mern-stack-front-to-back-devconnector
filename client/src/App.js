078005d9ffaf33f382bab66d845a45b2b33f30d4
Tiago Gomes <tidgomes@gmail.com>
2022-01-13 15:13:34 -0300

    filling the form fields in the Register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React, { Fragment } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
